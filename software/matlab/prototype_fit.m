a18caaa9ba3fa35c518d06c9d8aea6d4dee1d87a
Fernando Barbosa <fbcbarbosa@gmail.com>
2014-11-17 01:40:35 -0200

    Eagle Schematic

--- software/matlab/prototype_fit.m
+++ software/matlab/prototype_fit.m
@@ -1,21 +1,21 @@
 clear; clc;
+set(0, 'defaultTextInterpreter', 'Tex');
 
-%% CALIBRAÇÃO DO SENSOR DE PRESSÃO
-
+%% CALIBRAÇÃO DO SENSOR DE PRESSÃO - Regressão Linear
 mes = ...
-[0.02	0.03	0.01	0.02
-0.87	0.91	0.90    0.89
-1.70	1.72	1.70	1.72
-2.48	2.55	2.53	2.57
-3.37	3.36	3.39	3.40
-4.20	4.21	4.21	4.21
-5.06	5.07	5.07	5.05
-5.92	5.95	5.92	5.92
-6.68	6.75	6.70	6.72
-7.58	7.58	7.57	7.59
-8.37	8.43	8.40	8.41
-9.20	9.20    9.22	9.17
-10.02	10.02	10.01	10.03];
+[0.06	0.07	0.06	0.05
+0.82	0.95	0.94	0.87
+1.62	1.66	1.68	1.65
+2.40	2.48	2.53	2.45
+3.31	3.43	3.38	3.33
+4.15	4.16	4.14	4.13
+4.97	5.04	4.94	4.93
+5.84	5.86	5.77	5.88
+6.67	6.61	6.56	6.67
+7.44	7.47	7.38	7.39
+8.27	8.24	8.19	8.26
+9.07	9.01	9.01	9.00
+9.86	9.83	9.83	9.84];
 
 Y = mean(mes, 2);
 X = [0 20 40 60 80 100 120 140 160 180 200 220 240]';
@@ -24,9 +24,9 @@ lm = LinearModel.fit(X,Y)
 lm.plot()
 lm.anova()
 
-title('Função de transferência (V x P)')
-xlabel('P [mmHg]', 'FontSize', 12);
-ylabel('V [V]', 'FontSize', 12);
+title('')
+xlabel('Pressão [mmHg]', 'FontSize', 12);
+ylabel('Tensão Elétrica [V]', 'FontSize', 12);
 legend('Dados', 'Regressão Linear', 'Location', 'NorthWest');
 
 xlim([0,250]);
@@ -39,6 +39,74 @@ set(children(4),'Marker', '*')
 delete(children(1));
 grid on;
 
-% Y = 0.044478 + 0.041709*X
-b0 = -0.044478/0.041709
-b1 = 1/0.041709
+% Y = 0.060412 + 0.040867*X
+b0 = -0.060412/0.040867
+b1 = 1/0.040867
+
+% OUTPUT
+%
+% lm = 
+% 
+% 
+% Linear regression model:
+%     y ~ 1 + x1
+% 
+% Estimated Coefficients:
+%                    Estimate    SE            tStat     pValue    
+%     (Intercept)    0.060412       0.01662     3.635     0.0039223
+%     x1             0.040867    0.00011752    347.75    1.3962e-23
+% 
+% 
+% Number of observations: 13, Error degrees of freedom: 11
+% Root Mean Squared Error: 0.0317
+% R-squared: 1,  Adjusted R-Squared 1
+% F-statistic vs. constant model: 1.21e+05, p-value = 1.4e-23
+% 
+% ans = 
+% 
+%              SumSq       DF    MeanSq       F             pValue    
+%     x1         121.58     1       121.58    1.2093e+05    1.3962e-23
+%     Error    0.011059    11    0.0010054                            
+% 
+% 
+% b0 =
+% 
+%    -1.4783
+% 
+% 
+% b1 =
+% 
+%    24.4696
+
+%% CALIBRAÇÃO DO SENSOR DE PRESSÃO - Resíduos
+
+abs_residuals = abs(double(lm.Residuals(:,1)));
+
+bar(X, abs_residuals)
+
+xlabel('P [mmHg]', 'FontSize', 12);
+ylabel('Dif [V]', 'FontSize', 12);
+
+xlim([-10,250]);
+set(gca,'XTick',(0:20:240), 'FontSize', 12);
+grid on;
+
+max_dif = max(abs_residuals)
+span = max(Y)
+lin_error_pct = max_dif/span*100
+
+% OUTPUT
+%
+% max_dif =
+% 
+%     0.0557
+% 
+% 
+% span =
+% 
+%     9.8400
+% 
+% 
+% lin_error_pct =
+% 
+%     0.5665
